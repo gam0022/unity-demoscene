e97c072a8651b312b2f710a9515a2f09135c9511
gam0022 <gam0022@gmail.com>
2017-12-20 03:16:46 +0900

    Add DemoCameraWork

---
+++ Assets/Demo/DemoCameraWork.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Timeline;
+
+public class DemoCameraWork : MonoBehaviour, ITimeControl
+{
+    [SerializeField] private Transform target;
+
+    // 何度も呼ばれる
+    public void SetTime(double time)
+    {
+        transform.position = target.position + new Vector3(0.0f, 0.0f, 2.0f);
+        transform.LookAt(target);
+    }
+
+    // クリップ開始時に呼ばれる
+    public void OnControlTimeStart()
+    {
+    }
+
+    // クリップから抜ける時に呼ばれる
+    public void OnControlTimeStop()
+    {
+    }
+}
