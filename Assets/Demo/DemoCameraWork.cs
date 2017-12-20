03ffca22223fade2534eac08c205958df6e2d846
gam0022 <gam0022@gmail.com>
2017-12-20 22:54:22 +0900

    Imprve DemoCameraWork

--- Assets/Demo/DemoCameraWork.cs
+++ Assets/Demo/DemoCameraWork.cs
@@ -1,23 +1,44 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Timeline;
 
 public class DemoCameraWork : MonoBehaviour, ITimeControl
 {
     [SerializeField] private Transform target;
 
-    // 何度も呼ばれる
     public void SetTime(double time)
     {
-        transform.position = target.position + new Vector3(0.0f, 0.0f, 2.0f);
-        transform.LookAt(target);
+        float t = (float) time;
+        
+        if (t < 3f)
+        {
+            float rate = t / 3f;
+            transform.position = target.position + new Vector3(0f, 0f, Mathf.Lerp(2f, 1f, rate * rate));
+            transform.LookAt(target);
+        }
+        else if (t < 5f)
+        {
+            float rate = (t - 3f) / (5f - 3f);
+            transform.position = target.position + new Vector3(Mathf.Sin(rate * rate), 0f, Mathf.Cos(rate * rate));
+            transform.LookAt(target);
+        }
+        else if (t < 6f)
+        {
+            transform.position = new Vector3(4f, 2f, 20f);
+            transform.LookAt(target);
+        }
+        else
+        {
+            float rate = (t - 6f) / (10f - 6f);
+            transform.position = new Vector3(0f, Mathf.Lerp(2f, 20f, rate * rate), 20f);
+            transform.LookAt(new Vector3(0f, 0f, 25f + rate * 20f));
+        }
     }
 
-    // クリップ開始時に呼ばれる
     public void OnControlTimeStart()
     {
     }
 
-    // クリップから抜ける時に呼ばれる
     public void OnControlTimeStop()
     {
     }
