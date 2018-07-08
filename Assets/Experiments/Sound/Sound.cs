99eb0c246884cf5117aa523548d84a0a731277f0
gam0022 <gam0022@gmail.com>
2018-07-08 18:51:18 +0900

    Mod length

--- Assets/Experiments/Sound/Sound.cs
+++ Assets/Experiments/Sound/Sound.cs
@@ -8,10 +8,10 @@ public class Sound : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        int length = 600;
+        int length = 600 * 100;
         var se = gameObject.AddComponent<AudioSource>();
         (se.clip = AudioClip.Create("test",length, 1, 44100, false))
-            .SetData(Enumerable.Range(0, length).Select(t => Mathf.Sin(t * 0.2f)).ToArray(), 0);
+            .SetData(Enumerable.Range(0, length).Select(t => Mathf.Sin(t * 0.05f)).ToArray(), 0);
         se.loop = true;
         se.Play();
     }
