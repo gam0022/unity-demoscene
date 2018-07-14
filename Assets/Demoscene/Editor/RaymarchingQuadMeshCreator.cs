1f191e98a30704eb1e83bdce1bcfb0803e2557e2
gam0022 <gam0022@gmail.com>
2018-07-15 04:03:49 +0900

    RaymarchingQuadMeshCreator

---
+++ Assets/Demoscene/Editor/RaymarchingQuadMeshCreator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class RaymarchingQuadMeshCreator
+{
+    static readonly string outputPath = "Assets/Demoscene/Resources/Meshes/RaymarchingQuad.mesh";
+    private const int expandBounds = 10000;
+
+    [MenuItem("Tools/CreateRaymarchingQuadMesh")]
+    static void CreateRaymarchingQuadMesh()
+    {
+        var mesh = new Mesh
+        {
+            vertices = new[]
+            {
+                new Vector3(1f, 1f, 0f),
+                new Vector3(-1f, 1f, 0f),
+                new Vector3(-1f, -1f, 0f),
+                new Vector3(1f, -1f, 0f),
+            },
+            triangles = new[] {0, 1, 2, 2, 3, 0}
+        };
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+
+        var bounds = mesh.bounds;
+        bounds.Expand(expandBounds);
+        mesh.bounds = bounds;
+
+        var oldAsset = AssetDatabase.LoadAssetAtPath<Mesh>(outputPath);
+        if (oldAsset)
+        {
+            // Update Asset
+            EditorUtility.CopySerialized(mesh, oldAsset);
+            AssetDatabase.SaveAssets();
+        }
+        else
+        {
+            // Create Asset
+            AssetDatabase.CreateAsset(mesh, outputPath);
+            AssetDatabase.Refresh();
+        }
+    }
+}
