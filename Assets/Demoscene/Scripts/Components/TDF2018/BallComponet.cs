4d647658273a1186eb68dcc94d989b2596fed663
gam0022 <gam0022@gmail.com>
2018-11-11 21:42:02 +0900

    Add BallComponet

---
+++ Assets/Demoscene/Scripts/Components/TDF2018/BallComponet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Demoscene.TDF2018
+{
+    public class BallComponet : MonoBehaviour
+    {
+        readonly int timeId = Shader.PropertyToID("_LocalTime");
+        readonly int durationId = Shader.PropertyToID("_LocalDuration");
+
+        [SerializeField] Material material;
+
+        public void ApplyTime(float time, float duration)
+        {
+        }
+
+        public void ApplyPosition(Vector3 position)
+        {
+            transform.position = position;
+        }
+    }
+}
