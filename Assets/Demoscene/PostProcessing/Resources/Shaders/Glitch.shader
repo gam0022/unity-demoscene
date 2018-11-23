9d6c914fc4a7a8961c33e90a88a6a108a9970c4e
gam0022 <gam0022@gmail.com>
2018-11-23 16:26:23 +0900

    Create PostProcessing Glitch

---
+++ Assets/Demoscene/PostProcessing/Resources/Shaders/Glitch.shader
@@ -0,0 +1,34 @@
+Shader "Hidden/Demoscene/Glitch"
+{
+    HLSLINCLUDE
+
+        #include "Assets/PostProcessing/PostProcessing/Shaders/StdLib.hlsl"
+
+        TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+        float _Intensity;
+
+        float4 Frag(VaryingsDefault i) : SV_Target
+        {
+            float4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord);
+            float luminance = dot(color.rgb, float3(0.2126729, 0.7151522, 0.0721750));
+            color.rgb = lerp(color.rgb, luminance.xxx, _Intensity.xxx);
+            return color;
+        }
+
+    ENDHLSL
+
+    SubShader
+    {
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            HLSLPROGRAM
+
+                #pragma vertex VertDefault
+                #pragma fragment Frag
+
+            ENDHLSL
+        }
+    }
+}
---
+++ Assets/Demoscene/PostProcessing/Runtime/Effects/Glitch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace Demoscene.PostProcessing
+{
+    [Serializable]
+    [PostProcess(typeof(GlitchRenderer), PostProcessEvent.AfterStack, "Demoscene/Glitch")]
+    public sealed class Glitch : PostProcessEffectSettings
+    {
+        [Range(0f, 1f), Tooltip("Glitch effect intensity.")]
+        public FloatParameter intensity = new FloatParameter { value = 0.5f };
+
+        public override bool IsEnabledAndSupported(PostProcessRenderContext context)
+        {
+            return enabled.value && intensity.value > 0f;
+        }
+    }
+
+    public sealed class GlitchRenderer : PostProcessEffectRenderer<Glitch>
+    {
+        public override void Render(PostProcessRenderContext context)
+        {
+            var sheet = context.propertySheets.Get(Shader.Find("Hidden/Demoscene/Glitch"));
+            sheet.properties.SetFloat("_Intensity", settings.intensity);
+            context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+        }
+    }
+}
